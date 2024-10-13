39017f2ff122892455322e042142f848dfa896d5
xlpmyxhdr <90658478+gucooing@users.noreply.github.com>
2024-10-13 20:19:40 +0800

    up load ec2b

--- pkg/alg/getec2b.go
+++ pkg/alg/getec2b.go
@@ -13,15 +13,10 @@ func GetEc2b() *random.Ec2b {
 	open, err := os.Open("data/Ec2b.bin")
 	defer open.Close()
 	if err != nil {
-		ec2p := random.NewEc2b().Bytes()
-		ioutil.WriteFile("data/Ec2b.bin", ec2p, 0644)
+		ec2p := random.NewEc2b()
+		ioutil.WriteFile("data/Ec2b.bin", ec2p.Bytes(), 0644)
 		log.Println("ec2b不存在,生成ec2b文件中")
-		ec2b, err := random.LoadEc2bKey(ec2p)
-		if err != nil {
-			log.Printf("parse region ec2b error: %v\n", err)
-			return nil
-		}
-		return ec2b
+		return ec2p
 	} else {
 		ec2p, err := io.ReadAll(open)
 		if err != nil {
